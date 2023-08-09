8509072ed98a65a91f0ac933e8a3fb7f4b1b98de
Karina <kulinicheva_91@mail.ru>
2023-08-10 00:50:14 +0300

    passed test

--- src/test/java/netology/ru/PostmanEchoTest.java
+++ src/test/java/netology/ru/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then().log().all()
                 .statusCode(200)
-                .body("data", equalTo("Aleksandrg Pushkin"))
+                .body("data", equalTo("Aleksandr Pushkin"))
         ;
 
     }
